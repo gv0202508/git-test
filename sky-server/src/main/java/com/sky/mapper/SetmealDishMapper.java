5d97391247b47efb342ed9fdee5fc8dca382de2b
byh <byh@byh.cn>
2026-09-04 21:21:22 +0800

    新增套餐

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.sky.entity.SetmealDish;
+
 @Mapper
 public interface SetmealDishMapper {
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+    void insertBatch(List<SetmealDish> setmealDishes);
 }
