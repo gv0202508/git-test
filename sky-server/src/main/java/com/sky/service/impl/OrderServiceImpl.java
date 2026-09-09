8db839d9f54210a0689b53cc77d4fce496a8ae47
byh <byh@byh.cn>
2026-09-09 12:24:54 +0800

    催单

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -420,4 +420,21 @@ public class OrderServiceImpl implements OrderService{
 
         orderMapper.update(orders);
     }
+
+    public void reminder(Long id) {
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        Map map=new HashMap();
+        map.put("type", 2);
+        map.put("orderId", id);
+        map.put("content", "订单号"+ordersDB.getNumber());
+        String json=JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
+
+    }
 }
